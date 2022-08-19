5a73a8b92bd1bcbb12007d928b191d6b82529f47
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-08-19 11:37:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpPoint){
+    if(pickUpPoint > 42){
+        return pickUpPoint - 42;
+    }
+    else{
+        return 42 - pickUpPoint;
+    }    
+}
+// part 2
+function distanceFromHqInFeet(pickUpPoint) {
+    return distanceFromHqInBlocks(pickUpPoint)* 264;
+}
+// part 3
+function distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination-start)*264; // 264 feet per block
+    }
+    else{
+        return (start-destination) *264;
+    }
+}
+// part 4
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance < 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance >= 2000 && distance < 2500) {
+      return 25;
+    } else if (distance >= 2500) {
+      return "cannot travel that far";
+    }
+  }
+  calculatesFarePrice(43, 44);
+  calculatesFarePrice(50, 58);
+   calculatesFarePrice(34, 24);
